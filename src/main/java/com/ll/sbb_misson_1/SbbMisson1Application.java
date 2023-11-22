f4ccaf235270004a767b54b020326804ab7829ea
Yimin2 <gownfla14@naver.com>
2023-11-22 09:39:58 +0900

    2-1 start

---
+++ src/main/java/com/ll/sbb_misson_1/SbbMisson1Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbb_misson_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMisson1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbMisson1Application.class, args);
+    }
+
+}
