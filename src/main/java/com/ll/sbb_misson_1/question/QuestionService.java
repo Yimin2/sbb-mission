0617011e314cc680c4ad4d88d33151752ce0df30
Yimin2 <gownfla14@naver.com>
2023-11-29 10:26:54 +0900

    2-10 질문 상세

--- src/main/java/com/ll/sbb_misson_1/question/QuestionService.java
+++ src/main/java/com/ll/sbb_misson_1/question/QuestionService.java
@@ -1,9 +1,12 @@
 package com.ll.sbb_misson_1.question;
 
+import com.ll.sbb_misson_1.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.zip.DataFormatException;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +15,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException(("question not found"));
+        }
     }
 }
