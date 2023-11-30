92ad372aecdfabc67efc651369f47035531a9812
Yimin2 <gownfla14@naver.com>
2023-11-30 17:24:27 +0900

    answer 시간 수정

--- src/main/java/com/ll/sbb_misson_1/question/QuestionController.java
+++ src/main/java/com/ll/sbb_misson_1/question/QuestionController.java
@@ -1,12 +1,11 @@
 package com.ll.sbb_misson_1.question;
 
+import jakarta.validation.Valid;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +28,18 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question",question);
         return "question_detail";
+    }
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_Form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
     }
 }
